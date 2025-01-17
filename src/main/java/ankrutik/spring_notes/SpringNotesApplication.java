d7d57c55a4472113c63d9e93ff799c761b2407da
Krutik Arekar (MacBook Pro) <ankrutik@gmail.com>
2025-01-17 08:32:47 +0530

    component, componentscan, multiple contexts

--- src/main/java/ankrutik/spring_notes/SpringNotesApplication.java
+++ src/main/java/ankrutik/spring_notes/SpringNotesApplication.java
@@ -3,21 +3,40 @@ package ankrutik.spring_notes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ComponentScan;
 import ankrutik.spring_notes.config.Config1;
 import ankrutik.spring_notes.play.*;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import ankrutik.spring_notes.play.ShopFinder;
 
 @SpringBootApplication
+@Configuration
+@ComponentScan(basePackages = "ankrutik.spring_notes.play")
 public class SpringNotesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringNotesApplication.class, args);
+		testConfig1();
+		testSelfConfigSpringNotesApplication();
+	}
 
+	private static void testConfig1(){
+		System.out.println("Testing Config1");
 		try (AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Config1.class)) {
 			System.out.println(context.getBean("TuesdayPlan"));
 			System.out.println(context.getBean("mondayPlan"));
 			System.out.println(context.getBean("WednesdayPlan"));
 			
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+
+	private static void testSelfConfigSpringNotesApplication(){
+		System.out.println("Testing SelfConfigSpringNotesApplication");
+		try (AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SpringNotesApplication.class)) {
+			System.out.println(context.getBean(ShopFinder.class));
 		} catch (Exception e) {
 			System.out.println(e);
 		}
---
+++ src/main/java/ankrutik/spring_notes/play/ShopFinder.java
@@ -0,0 +1,11 @@
+package ankrutik.spring_notes.play;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ShopFinder {
+    @Override
+    public String toString() {
+        return "ShopFinder Basic";
+    }
+}
